7669304f06aa6d9379577c55f36061e57c7bff84
matthew <cmatthew.park@gmail.com>
2024-10-10 13:02:28 -0500

    Update Lab1.py

--- labs/Lab1.py
+++ labs/Lab1.py
@@ -1,7 +1,9 @@
-# Name:
-# Period:
-# Date:
+# Name: matthew
+# Period:6th
+# Date:10/10
     
 # define a function isLeapYear() that takes a given year as
 # a parameter and returns true if the given year is a leap
 # year and false if the given year is not a leap year.
+def isLeapYear(year):
+    if year % 4 == 0 or year % 400 ==0P:
